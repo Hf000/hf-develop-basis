2860d0d337e7223cb8900d2d2b16bc056e6b095a
HF-PC\HF <809969840@qq.com>
2021-02-21 19:21:40 +0800

    hf update

---
+++ mybatis-parent/mybatis-plus-spring-simple/src/main/java/com/hufei/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.hufei.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hufei.pojo.po.User;
+
+/**
+ * <p>  </p>
+ *
+ * @author hufei
+ * @version 1.0.0
+ * @date 2021/2/21 19:09
+ */
+public interface UserMapper extends BaseMapper<User> {
+}
---
+++ mybatis-parent/mybatis-plus-spring-simple/src/main/java/com/hufei/pojo/po/User.java
@@ -0,0 +1,28 @@
+package com.hufei.pojo.po;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * <p> 用户实体 </p>
+ *
+ * @author hufei
+ * @date 2021/2/21 17:43
+ * @version 1.0.0
+ */
+@Data
+//注解在类上，为类提供一个无参的构造
+@NoArgsConstructor
+//为类提供一个全参的构造
+@AllArgsConstructor
+@TableName("tb_user")
+public class User {
+    private Long id;
+    private String userName;
+    private String password;
+    private String name;
+    private Integer age;
+    private String email;
+}
